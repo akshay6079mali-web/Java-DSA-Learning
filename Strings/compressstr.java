8e9509d52c967474139351151c4bb3e235c5a6c0
akshay6079-web <akshay6079mali@gmail.com>
2026-03-21 17:59:25 +0530

    Added Program to compress a string

---
+++ Strings/compressstr.java
@@ -0,0 +1,14 @@
+public class compressstr {
+    public static void main (String [] args){
+        StringBuilder sb = new StringBuilder("aaabbcccd");
+
+        for(int i = 0; i < sb.length() ; i++){
+            int count = 1;
+            while(i < sb.length() - 1 && sb.charAt(i) == sb.charAt(i + 1)){
+                count++;
+                i++;
+            }
+            System.out.print(sb.charAt(i) + "" + count + " ");
+        }
+    }
+}
